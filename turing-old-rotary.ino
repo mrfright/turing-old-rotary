027d64fe96b4ef804fae927d8997badacf07caa2
Eric Faehnrich <eric.faehnrich@paragon-inc.com>
2015-05-17 22:07:32 -0400

    plays WAV file when its code is entered

--- turing-old-rotary.ino
+++ turing-old-rotary.ino
@@ -12,6 +12,7 @@ Coded by Eric Faehnrich
 
 enum State {
   nodial,
+  dialing,
   dialed,
   done
 };
@@ -28,17 +29,35 @@ uint8_t dirLevel; // indent level for file/dir names    (for prettyprinting)
 dir_t dirBuf;     // buffer for directory reads
 
 const int hangPin = 6; //pin for reciever switch. closed (HIGH) when hung up, open (LOW) when off the hook
-
+const int dialPin = 7; //pin for dialing switch. close (HIGH) when  dialing, open (LOW) when not dialing
+const int countPin = 8; //pin for counting switch.  closed (HIGH) for don't count, open (LOW) for each count
 const int ledPin = 13; //LED pin
+
+int dialCount = 0;
+#define DIAL_ARRAY_LEN 10
+int dialArray[DIAL_ARRAY_LEN];
+int dialIndex = 0;
+
+int p1[] = {1, 2, 3, 4};
+const int p1_len = 4;
+int p2[] = {0, 9, 8, 7};
+const int p2_len = 4;
 
 int ledState = HIGH;
 int hangState; //current reciever reading
+int dialState; //current dial reading
+int countState;
 int lastHangState = LOW; //previous reciever reading
+int lastDialState = LOW; //previous dial reading
+int lastCountState = LOW;
 
 long lastHangDebounceTime = 0;  // the last time the reciever pin was toggled
+long lastDialDebounceTime = 0; // last time the dial switch was toggled
+long lastCountDebounceTime = 0;
 const long debounceDelay = 50;    // the debounce time; increase if the output flickers
+const long countDebounceDelay = 10;
 
-State dialState = nodial;
+State phoneState = nodial;
 
 void playfile(char *name) {
   if (wave.isplaying) {
@@ -105,6 +124,8 @@ void setup() {
   
   
   pinMode(hangPin, INPUT);
+  pinMode(dialPin, INPUT);
+  pinMode(countPin, INPUT);
   pinMode(ledPin, OUTPUT);
 
   digitalWrite(ledPin, ledState);
@@ -112,6 +133,8 @@ void setup() {
 
 void loop() {
   int hangReading = digitalRead(hangPin);
+  int dialReading = digitalRead(dialPin);
+  int countReading = digitalRead(countPin);
   
   //debounce the reciever switch
   if (hangReading != lastHangState) {
@@ -121,29 +144,18 @@ void loop() {
   if ((millis() - lastHangDebounceTime) > debounceDelay) { //then the bounce settled
     if (hangReading != hangState) {//then reciever state has changed
       hangState = hangReading;
-      ledState = hangState;   
       
       if(hangState == LOW) {//if first time off hook, start dial tone
-        dialState = nodial;
+        phoneState = nodial;
+        putstring_nl("hang low");
       }   
+      else {
+        putstring_nl("hang high");
+      }
     }
   }
   
-  //debounce the reciever switch
-  if (hangReading != lastHangState) {
-    lastHangDebounceTime = millis();
-  }
-
-  if ((millis() - lastHangDebounceTime) > debounceDelay) { //then the bounce settled
-    if (hangReading != hangState) {//then reciever state has changed
-      hangState = hangReading;
-      ledState = hangState;   
-      
-      if(hangState == LOW) {//if first time off hook, start dial tone
-        dialState = nodial;
-      }   
-    }
-  }
+  
 
   digitalWrite(ledPin, ledState);
 
@@ -153,9 +165,110 @@ void loop() {
     //stop any playback
     wave.stop();
     //reset any state
+    dialCount = 0;
+    dialIndex = 0;
+    
+    int i = 0;
+    for(i = 0; i < DIAL_ARRAY_LEN; i++) {
+      dialArray[i] = -1;
+    }
   }
   else {//off the hook
-    switch(dialState) {
+    //debounce the dial switch
+    if (dialReading != lastDialState) {
+      lastDialDebounceTime = millis();
+    }
+  
+    if ((millis() - lastDialDebounceTime) > debounceDelay) { //then the bounce settled
+      if (dialReading != dialState) {//then reciever state has changed
+        dialState = dialReading; 
+        ledState = dialState;
+        
+        if(dialState == LOW) {
+          putstring("dial low");
+          if(dialReading == HIGH) {
+            putstring("\tdial reading high");
+          }
+          else {
+            putstring("\tdial reading high\n");
+          }
+        }
+        
+        //if just started dialing, reset count
+        if(dialState == HIGH) {
+          dialCount = 0;
+          putstring_nl("dial high");
+          
+
+        }
+        //if just ended dialing, save count, set to dialing state if recied a count > 0 (10 is the real zero)
+        //if dialindex bigger than dial array size, then act like wrong number
+        //if dialed number matches anything then play
+        else if(dialCount > 0) {
+          int i;
+          phoneState = dialing;
+          dialArray[dialIndex] = dialCount;
+          if(dialArray[dialIndex] >= 10) {
+            dialArray[dialIndex] = 0;
+          }
+          dialIndex++;
+          
+          i = 0;
+          int match = 1;
+          for(i = 0; i < p1_len; i++) {
+            if(p1[i] != dialArray[i]) {
+              match = 0;
+              break;
+            }
+          }
+          
+          if(match) {
+            playfile("P1.WAV");
+          }
+          
+          i = 0;
+          match = 1;
+          for(i = 0; i < p2_len; i++) {
+            if(p2[i] != dialArray[i]) {
+              match = 0;
+              break;
+            }
+          }
+          
+          if(match) {
+            playfile("P2.WAV");
+          }
+          
+        }  
+      }
+    }
+    
+    if(dialState == HIGH) {
+      //debounce the count switch
+      if (countReading != lastCountState) {
+        lastCountDebounceTime = millis();
+      }
+    
+      if ((millis() - lastCountDebounceTime) > countDebounceDelay) { //then the bounce settled
+        if (countReading != countState) {//then reciever state has changed
+          countState = countReading; 
+          
+          
+          //if just started dialing, reset count
+          if(countState == LOW) {
+            dialCount++;
+            putstring_nl("count low");
+  
+          }
+          else {
+            putstring_nl("count high");
+          }
+
+        }
+      }
+    }
+    
+    switch(phoneState) {
       case nodial:
         if(!wave.isplaying) {//if stopped playing, start again
           playfile("DIALTONE.WAV");
@@ -165,5 +278,7 @@ void loop() {
   }
   
   lastHangState = hangReading;
+  lastDialState = dialReading;
+  lastCountState = countReading;
 }
 
